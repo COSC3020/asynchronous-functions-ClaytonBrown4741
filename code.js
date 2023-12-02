80c798c87b27a888946a5e37934f2df5ca09ba4f
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-12-02 10:36:13 -0700

    Updated code

--- code.js
+++ code.js
@@ -1,20 +1,27 @@
 var finalAnswer=-1
-function nmPMR(arr, key, cb) {
+var currentKey=-1
+var manualAnswer=0;
+
+
+function nmPMR(arr, key, cb, infoArray) {
 	finalAnswer=-1
+	manualAnswer=0
+	for (var i=0; i<arr.length; i++){
+                if (arr[i]==key){
+                        manualAnswer=manualAnswer + 1
+                }
+        }
 	var Parallel = require('paralleljs'),
 	p = new Parallel(arr);
 	var acc=1;
 	global.process.env.key = key;
-		p.map(function(i) { return i == global.process.env.key; }).reduce(function(d) { return d[0] +d[1]; }).then(cb)
-
-	//while (finalAnswer==-1){
-	
-	//}
-	//return finalAnswer
+	p.map(function(i) { return i == global.process.env.key; }).reduce(function(d) { return d[0] +d[1]; }).then(cb)
 }
 
 function endThis(endValue){
 	finalAnswer=endValue
-	console.log(finalAnswer)
+	if (finalAnswer != manualAnswer){
+		console.log("WARNING! ERROR HAS BEEN ENCOUNTERED!")
+		console.log(finalAnswer + " is not the equal to " + manualAnswer)
+	}
 }
-console.log(nmPMR([1,1,1,1,0,1], 1, endThis))
