27f42b8e36cebcde7251aebc2637fdfe4c91ac2a
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-12-04 16:54:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,27 +1,9 @@
-var finalAnswer=-1
-var currentKey=-1
-var manualAnswer=0;
-
-
-function nmPMR(arr, key, cb, infoArray) {
-	finalAnswer=-1
-	manualAnswer=0
-	for (var i=0; i<arr.length; i++){
-                if (arr[i]==key){
-                        manualAnswer=manualAnswer + 1
-                }
-        }
+function nmPMR(arr, key, cb) {
+	if (arr.length==0)
+		return 0
+	
 	var Parallel = require('paralleljs'),
 	p = new Parallel(arr);
-	var acc=1;
 	global.process.env.key = key;
-	p.map(function(i) { return i == global.process.env.key; }).reduce(function(d) { return d[0] +d[1]; }).then(cb)
-}
-
-function endThis(endValue){
-	finalAnswer=endValue
-	if (finalAnswer != manualAnswer){
-		console.log("WARNING! ERROR HAS BEEN ENCOUNTERED!")
-		console.log(finalAnswer + " is not the equal to " + manualAnswer)
-	}
+	p.map(function(i) { if (i == global.process.env.key) return 1; else return 0; }).reduce(function(d) { return d[0] +d[1]; }).then(cb)
 }
