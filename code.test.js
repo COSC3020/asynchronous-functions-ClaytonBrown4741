88a72b6f8c3d89e97bacfcf1bd4c65dd3adb093d
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-12-04 16:55:03 -0700

    Added proper test cases

--- code.test.js
+++ code.test.js
@@ -1,5 +1,60 @@
 const fs = require('fs');
+const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
-
 dm1 = [1,1,1,1,2,1,3,5,3,2,2,1,2,3,4,4,3,2,1];
-nmPMR(dm1, 1, endThis)
+
+function cb1(endValue){
+	if (endValue != 7){
+		console.log("ERROR: THE END VALUE IS NOT THE CORRECT VALUE OF 7")
+	}
+}
+
+nmPMR(dm1, 1, cb1)
+
+
+dm2 = [1,1,1,1,2,1,3,5,3,2,2,1,2,3,4,4,3,2,1];
+function cb2(endValue){
+        if (endValue != 4){
+                console.log("ERROR: THE END VALUE IS NOT THE CORRECT VALUE OF 4")
+        }
+}
+
+nmPMR(dm2, 3, cb2)
+
+
+dm3 = [1];
+function cb3(endValue){
+        if (endValue != 0){
+		console.log(endValue)
+                console.log("ERROR: THE END VALUE IS NOT THE CORRECT VALUE OF 0")
+        }
+}
+nmPMR(dm3, 3, cb3)
+
+
+dm4 = [1,1,1,1,2,1,3,5,3,2,2,1,2,3,4,4,3,2,1];
+function cb4(endValue){
+        if (endValue != 0){
+                console.log("ERROR: THE END VALUE IS NOT THE CORRECT VALUE OF 0")
+        }
+}
+nmPMR(dm4, 18, cb4)
+
+
+dm5 = [5,5,5,5,5,5,5,5,5,5,5,5];
+function cb5(endValue){
+        if (endValue != 12){
+                console.log("ERROR: THE END VALUE IS NOT THE CORRECT VALUE OF 12")
+        }
+}
+nmPMR(dm5, 5, cb5)
+
+
+dm6 = [];
+function cb6(endValue){
+        if (endValue != 0){
+                console.log(endValue)
+                console.log("ERROR: THE END VALUE IS NOT THE CORRECT VALUE OF 0")
+        }
+}
+nmPMR(dm6, 3, cb6)
